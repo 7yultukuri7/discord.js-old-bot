24925dcdcb4025787c09835746a40e100e5a8cd5
Glitch (7yultukuri7discordjsbot) <none>
2019-08-26 08:57:21 +0000

    💋🎐 Updated with Glitch

--- commands/bot/webshot.js
+++ commands/bot/webshot.js
@@ -26,38 +26,11 @@ module.exports = class extends Command {
 
     run(message, {url}) {
 (async () => {
-  //ブラウザを定義(headless:false ブラウザを表示する, true 表示しない)
-  const browser = await puppeteer.launch({ headless: false });
-  //タブを定義
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  //ブラウザのサイズを定義
-  await page.setViewport({width: 1240, height:1080});
-
-  //待機
-  async function sleep(delay) {
-    return new Promise(resolve => setTimeout(resolve, delay));
-  }
-
-  // Googleにアクセス
-  await page.goto('https://www.google.co.jp/');
-  // 検索窓に「ラクス　エンジニアブログ」と入力
-  await page.type('.gLFyf', 'ラクス　エンジニアブログ');
-  //スクリーンショット撮影
-  await page.screenshot({path: 'google.png'});
-  // 検索ボタンクリック
-  //待機時間を設けないと止まってしまうことがあるので記述
-  await sleep(5000);
-  await page.focus('input[name="btnK"]');
-  await page.click('input[name="btnK"]');
-  //待機時間を設けないと止まってしまうことがあるので記述
-  await sleep(5000);
-  //スクリーンショット撮影
-  await page.screenshot({path: 'search_result.png'});
-  // 検索結果の先頭リンクをクリック
-  await page.click('.rc > .r > a');
-  //スクリーンショット撮影
-  await page.screenshot({path: 'blog.png'});
-  //ブラウザを閉じる
+  await page.goto('https://example.com');
+  await page.screenshot({path: 'example.png'});
+ 
   await browser.close();
 })();
         return
