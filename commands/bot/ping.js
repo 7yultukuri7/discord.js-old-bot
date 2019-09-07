466a93ab093f454c87504dffb48920e881fe679a
Glitch (7yultukuri7discordjsbot) <none>
2019-09-07 08:54:35 +0000

    📚👟 Updated with Glitch

---
+++ commands/bot/ping.js
@@ -0,0 +1,18 @@
+const { Command } = require("discord.js-commando");
+
+module.exports = class PingCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "ping",
+            group: "bot",
+            memberName: "ping",
+            description: "Pingを表示します",
+            guildOnly: true,
+        });
+    }
+
+    run(message) {
+        const ping = this.client.ping;
+        return message.say(`${ping}ms`);
+    }
+};
