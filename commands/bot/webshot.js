8b9715009b18a0fec61face7a27a9f94954295f0
Glitch (7yultukuri7discordjsbot) <none>
2019-10-01 11:51:22 +0000

    😇⛽️ Updated with Glitch

--- commands/bot/webshot.js
+++ commands/bot/webshot.js
@@ -1,7 +1,9 @@
 const { Command } = require("discord.js-commando");
-const { RichEmbed, discord } = require('discord.js');
+const discord = require('discord.js');
 const webshot = require('webshot');
-
+const puppeteer = require('puppeteer');
+const path = require('path');
+const moment = require("moment-timezone");
 
  const config = require("/app/config/main.js");
 
@@ -13,6 +15,7 @@ const webshot = require('webshot');
             group: "bot",
             memberName: "webshot",
             description: "ウェブショットをします。",
+            ownerOnly: true,
             guildOnly: true,
             args: [
                 {
@@ -25,27 +28,60 @@ const webshot = require('webshot');
     }
 
      run(message, {url}) {
-    const options = {
-      renderDelay: 10,
-      screenSize: {
-        width: 1920,
-        height: 1080
-      },
-      shotSize: {
-        width: 1920,
-        height: 'all'
-      },
-      userAgent: 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
-      defaultWhiteBackground: true
-    };
-
+/*(async () => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+//    executablePath: path.resolve('/user/bin/chromium-browser'),
+    headless: false
+  });
+  var page = await browser.newPage();
+  				page.on("error", error => {
+					message.channel.send(`:warning: ${error.message}`);
+				});
+  await page.setViewport({with: 1920, hight: 1080});
+  await page.goto(url);
+  var screenshot = await page.screenshot({type: 'png'});
+  await browser.close();
      const attachment = message.channel.send({
       files: [{
-        attachment: webshot(url, options),
+        attachment: screenshot,
         name: 'web.jpg'
       }]
     });
     message.channel.send(`webshot`, attachment);
+})();*/
+const grabPage = async () => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    
+  });
+	const page = await browser.newPage()
+	await page.goto(url)
+/*	const el = await page.$('#contents')
+	const buffer = await el.screenshot({ path: `${Date.now()}.png` })*/
+	const buffer = await page.screenshot({ path: `${Date.now()}.png` })
+
+	await browser.close()
+	return buffer
+}
+(async () => {
+		console.log('message matches, fetching')
+		// We can create embeds using the MessageEmbed constructor
+		// Read more about all that you can do with the constructor
+		// over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
+		const buffer = await grabPage()
+		const embed = new discord.RichEmbed()
+			// Set the title of the field
+			.setTitle('A slick little embed')
+			// Set the color of the embed
+			.setColor(0xff0000)
+			// Set the main content of the embed
+			.setDescription('Hello, this is a slick embed!')
+			.attachFile(buffer)
+		console.log('message sent')
+		// Send the embed to the same channel as the message
+		message.channel.send(embed)  
+})();
         return
   }
 };
--- system/enter-ad-block.js
+++ system/enter-ad-block.js
@@ -1,4 +1,5 @@
-const { RichEmbed, discord } = require('discord.js');
+const { Client, RichEmbed, discord } = require('discord.js');
+const moment = require("moment-timezone");
 const counter = function(str,seq){
     return str.split(seq).length - 1;
 }
@@ -6,16 +7,41 @@ const counter = function(str,seq){
 module.exports = (client, message) => {
   if (message.author.bot) return;
   if (message.channel.type == 'dm') return;
-if (message.guild.id === '573350684372762624'){
-if (11 <= counter(message.content,"\n")){
- console.log(message.content+":"+counter(message.content,"\n"));
-        const embedada = new discord.RichEmbed()
-          .setAuthor('1')
-          .setDescription('11')
-          .setColor(Math.floor(Math.random() * 16777214) + 1)
-          .setFooter(message.guild.name, message.guild.iconURL)
-          .setTimestamp();
-      message.channel.send("embedada");
+if (message.guild.id === '543615084618842132'){
+if (message.channel.id === '573682521229885451'){
+if (9 <= counter(message.content,"\n")){
+        var embedada = {
+	color: Math.floor(Math.random() * 16777214) + 1,
+	author: {
+		name: message.author.username + " (id: " + message.author.id + ")",
+		icon_url: message.author.avatarURL,
+	},
+	description: '送信したメッセージは、5秒後削除されます。\nこの注意メッセージは、15秒後削除されます。',
+	fields: [
+		{
+			name: ':warning: 注意！',
+			value: 'このチャンネルは、改行できる回数が制限されています。\n\u200b',
+		},
+		{
+			name: "このチャンネル(#"+message.channel.name + ')が改行できる回数',
+			value: '1~10',
+			inline: true,
+		},
+	],
+	timestamp: new Date(),
+	footer: {
+		text: message.guild.name + " (id: " + message.guild.id + ")",
+	},
+};
+                  message.delete(5000)
+      message.channel.send({ embed: embedada }).then(sent => { // 'sent' is that message you just sent
+                    let msgid = sent.id;
+                    let chid = sent.channel.id;
+const momentmsg = moment(message.createdAt).tz("Asia/Tokyo").format("HH:mm:ss");
+                  sent.delete(15000)
+                    client.channels.get("610706001980817418").send("`["+ momentmsg +"]` `[--]` :warning: **"+message.author.tag+"** (ID:"+message.author.id+")\n`[ Reason ]` <#"+message.channel.id+">のチャンネルで、改行回数制限 1~10");
+                    });
+};
 };
 };
   
